a65a06e132b3332a9a07aa16da7580fa5d50e0a2
Tetyana Shvets <tshvets@hotmail.com>
2019-02-03 10:51:24 -0800

    Notifications for new messages

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Menu, Icon, Modal, Form, Input, Button } from 'semantic-ui-react';
+import { Menu, Icon, Modal, Form, Input, Button, Label } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { setCurrentChannel, setPrivateChannel } from '../../actions';
 import firebase from '../../firebase';
@@ -9,10 +9,13 @@ class Channels extends Component {
   state = {
     user: this.props.currentUser,
     activeChannel: '',
+    channel: [],
     channels: [],
     channelName: '',
     channelDetails: '',
     channelsRef: firebase.database().ref('channels'),
+    messagesRef: firebase.database().ref('messages'),
+    notifications: [],
     modal: false,
     firstLoad: true
   }
@@ -30,7 +33,39 @@ class Channels extends Component {
     this.state.channelsRef.on('child_added', snap => {
       loadedChannels.push(snap.val());
       this.setState({ channels: loadedChannels}, () => this.setFirstChannel());
+      this.addNotificationListener(snap.key);
     })
+  }
+
+  addNotificationListener = channelId => {
+    this.state.messagesRef.child(channelId).on('value', snap => {
+      if(this.state.channel) {
+        this.handleNotifications(channelId, this.state.channel.id, this.state.notifications, snap);
+      }
+    })
+  }
+
+  handleNotifications = (channelId, currentChannelId, notifications, snap) => {
+    let lastTotal = 0;
+    let index = notifications.findIndex(notification => notification.id === channelId);
+    if(index !== -1) {
+      if(channelId !== currentChannelId) {
+        lastTotal = notifications[index].total;
+        if(snap.numChildren() - lastTotal > 0) {
+          notifications[index].count = snap.numChildren() - lastTotal;
+        }
+      }
+      notifications[index].lastKnownTotal = snap.numChildren();
+    } else {
+      notifications.push({
+        id: channelId,
+        total: snap.numChildren(),
+        lastKnownTotal: snap.numChildren(),
+        count: 0
+      })
+    }
+
+    this.setState({ notifications });
   }
 
   removeListeners = () => {
@@ -42,7 +77,7 @@ class Channels extends Component {
       const firstChannel = this.state.channels[0];
       this.props.setCurrentChannel(firstChannel);
       this.setActiveChannel(firstChannel);
-      this.setState({ firstLoad: false});
+      this.setState({ firstLoad: false, channel: firstChannel });
     }
   }
 
@@ -90,12 +125,34 @@ class Channels extends Component {
 
   changeChannel = channel => {
     this.setActiveChannel(channel);
+    this.clearNotifications();
     this.props.setCurrentChannel(channel);
     this.props.setPrivateChannel(false);
+    this.setState({ channel });
+  }
+
+  clearNotifications = () => {
+    let index = this.state.notifications.findIndex(notification => notification.id === this.state.channel.id);
+    if(index !== -1) {
+      let updatedNotifications = [...this.state.notifications];
+      updatedNotifications[index].total = this.state.notifications[index].lastKnownTotal;
+      updatedNotifications[index].count = 0;
+      this.setState({ notifications: updatedNotifications});
+    }
   }
 
   setActiveChannel = channel => {
     this.setState({ activeChannel: channel.id })
+  }
+
+  getNotificationCount = channel => {
+    let count = 0;
+    this.state.notifications.forEach(notification => {
+      if(notification.id === channel.id) {
+        count = notification.count;
+      }
+    });
+    if(count > 0) return count;
   }
 
   displayChannels = channels => (
@@ -107,6 +164,9 @@ class Channels extends Component {
         style={{ opacity: 0.7}}
         active={channel.id === this.state.activeChannel}
       >
+        {this.getNotificationCount(channel) && (
+          <Label color='red'>{this.getNotificationCount(channel)}</Label>
+        )}
         # {channel.name}
       </Menu.Item>
     ))
